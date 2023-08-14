8023927e6182d0b8e0877a39dfb995b3c92d9856
Iago martins <martinsiagosaraiva@gmail.com>
2023-08-14 14:37:34 -0300

    new components feature

--- src/app/components/Business.jsx
+++ src/app/components/Business.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { features } from '../constants';
 import styles, { layout } from '../style';
 import Button from './Button';
@@ -11,7 +12,7 @@ const FeatureCard = ({ icon, title, content, index }) => (
     <div
       className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
     >
-      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+      <Image src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
     </div>
     <div className="flex-1 flex flex-col ml-3">
       <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
